1cd5b25d5b46b7f58a34b4567829e3a01a60cc43
maxime ♥✿ <maxime.grandclaude@edu.esiee.fr>
2024-09-16 13:14:20 +0000

    Code qui détecte si une liste de chaîne de caractères est un palindrome.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #### Fonction secondaire
-
-
+"""Programme qui vérifie si une liste de caractères est
+ un palindrome à l'aide d'une fonction auxiliaire"""
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """Test si la chaine de caractère p est un palindrome"""
+    p = p.lower()
+    table = str.maketrans("éèêëàîïçûüùôö","eeeeaiicuuuoo"," '-_:;?!,&")
+    p = p.translate(table)
+    if p == p[::-1]:
+        return True
     return False
-
 #### Fonction principale
 
 
 def main():
-
+    """Teste si les mots de la liste de caractères sont des palindromes"""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
